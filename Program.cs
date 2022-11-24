8922149618e9f0747d9753ba08bf5767bef5d2f5
unknown <kvkvachev@yandex.ru>
2022-11-24 20:33:42 +0300

    задача 29

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача № 29
+/* int[] array =new int[8];
+Mass(array);
+Console.WriteLine($"Массив : {String.Join(" ", array)}");
+int [] Mass(int[] array){
+    for (int i=0 ; i<array.Length;i++){
+        array[i]=new 
+        Random().Next(0,100000000);
+    }
+    return array;
+}*/
