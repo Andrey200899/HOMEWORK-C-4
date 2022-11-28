6603ec8ec135f4c1c5f873a4e2a1d68563efc39e
unknown <kvkvachev@yandex.ru>
2022-11-28 12:22:19 +0300

    Задача №27

--- Program.cs
+++ Program.cs
@@ -9,3 +9,16 @@ int [] Mass(int[] array){
     }
     return array;
 }*/
+
+// Задача № 27
+Console.WriteLine("Введите число");
+int i = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+while (i > 0)
+{
+int num = i % 10;
+i = i / 10;
+sum = sum + num;
+}
+Console.WriteLine("Cумма всех цифр в числе равна: " + sum);
