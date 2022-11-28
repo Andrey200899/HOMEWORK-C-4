242d6f38883a82257dc1e51752706abf84d93a88
unknown <kvkvachev@yandex.ru>
2022-11-28 12:34:31 +0300

    Задача №25

--- Program.cs
+++ Program.cs
@@ -11,14 +11,25 @@ int [] Mass(int[] array){
 }*/
 
 // Задача № 27
-Console.WriteLine("Введите число");
-int i = Convert.ToInt32(Console.ReadLine());
-int sum = 0;
+//Console.WriteLine("Введите число");
+//int i = Convert.ToInt32(Console.ReadLine());
+//int sum = 0;
 
-while (i > 0)
-{
-int num = i % 10;
-i = i / 10;
-sum = sum + num;
-}
-Console.WriteLine("Cумма всех цифр в числе равна: " + sum);
+//while (i > 0)
+//{
+//int num = i % 10;
+//i = i / 10;
+//sum = sum + num;
+//}
+//Console.WriteLine("Cумма всех цифр в числе равна: " + sum);
+
+// Задача №25
+Console.WriteLine("Введите число A :");
+int a =Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B :");
+int b =Convert.ToInt32(Console.ReadLine());
+ int number =a;
+ for ( int i=1;i<b;i++){
+    number =number*a;
+ }
+ Console.WriteLine("A в степени B равно: " + number);
